cd313f8c8174acc86d266c91b0e2cef9aef93be0
Mateusz2333 <mateusz.nowak1@microsoft.wsei.edu.pl>
2024-11-03 18:18:43 +0100

    3.8

--- lab01/src/data/AppReducer.js
+++ lab01/src/data/AppReducer.js
@@ -5,18 +5,21 @@ export default function AppReducer(state, action) {
             alert(`Edytowanie osoby o id: ${action.id}`);
             return state;
         }
+        
         case "delete": {
             
-            return state.filter((item) => item.id !== action.id);
+            return state.filter(item => item.id !== action.id);
         }
+        
         case "rate": {
             
-            return state.map((item) =>
-                item.id === action.id
+            return state.map(item => 
+                item.id === action.id 
                     ? { ...item, rating: item.rating === 10 ? 0 : item.rating + 1 }
                     : item
             );
         }
+
         default:
             return state;
     }
