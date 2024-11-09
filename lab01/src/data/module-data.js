cee2fcb37e8883267d33c718fcd3d823535bb751
Mateusz2333 <mateusz.nowak1@microsoft.wsei.edu.pl>
2024-11-09 17:39:36 +0100

    4.1.3

--- lab01/src/data/module-data.js
+++ lab01/src/data/module-data.js
@@ -1,82 +1,82 @@
 export const data = [
   {
     "id": 0,
-    "name": "Katarzyna",
-    "car": "Kia",
-    "birth": "2000-01-01",
-    "eyes": "Niebieski",
-    "registration": "EF9012"
+    "name": "Tomasz",
+    "car": "Toyota",
+    "birth": "1990-10-10",
+    "eyes": "Zielony",
+    "registration": "AB1234"
   },
   {
     "id": 1,
-    "name": "Monika",
-    "car": "Toyota",
-    "birth": "1995-05-05",
-    "eyes": "Niebieski",
+    "name": "Ewelina",
+    "car": "Peugeot",
+    "birth": "2000-01-01",
+    "eyes": "Brązowy",
     "registration": "EF9012"
   },
   {
     "id": 2,
-    "name": "Anna",
-    "car": "Peugeot",
-    "birth": "2000-01-01",
-    "eyes": "Zielony",
+    "name": "Piotr",
+    "car": "Mercedes",
+    "birth": "1995-05-05",
+    "eyes": "Niebieski",
     "registration": "CD5678"
   },
   {
     "id": 3,
-    "name": "Julia",
+    "name": "Stanisław",
     "car": "Nissan",
-    "birth": "1990-10-10",
-    "eyes": "Zielony",
-    "registration": "AB1234"
-  },
-  {
-    "id": 4,
-    "name": "Tomasz",
-    "car": "BMW",
-    "birth": "2000-01-01",
+    "birth": "1995-05-05",
     "eyes": "Zielony",
     "registration": "EF9012"
   },
   {
-    "id": 5,
-    "name": "Tomasz",
-    "car": "Ford",
-    "birth": "1995-05-05",
-    "eyes": "Zielony",
-    "registration": "CD5678"
-  },
-  {
-    "id": 6,
-    "name": "Katarzyna",
-    "car": "Audi",
-    "birth": "2000-01-01",
-    "eyes": "Zielony",
-    "registration": "CD5678"
-  },
-  {
-    "id": 7,
-    "name": "Adam",
+    "id": 4,
+    "name": "Karol",
     "car": "Toyota",
     "birth": "2000-01-01",
     "eyes": "Niebieski",
     "registration": "EF9012"
   },
   {
-    "id": 8,
-    "name": "Julia",
+    "id": 5,
+    "name": "Monika",
     "car": "Ford",
     "birth": "2000-01-01",
+    "eyes": "Brązowy",
+    "registration": "AB1234"
+  },
+  {
+    "id": 6,
+    "name": "Stanisław",
+    "car": "Ford",
+    "birth": "1995-05-05",
     "eyes": "Zielony",
+    "registration": "EF9012"
+  },
+  {
+    "id": 7,
+    "name": "Karol",
+    "car": "Peugeot",
+    "birth": "1990-10-10",
+    "eyes": "Niebieski",
     "registration": "CD5678"
   },
   {
-    "id": 9,
-    "name": "Adam",
-    "car": "Peugeot",
+    "id": 8,
+    "name": "Anna",
+    "car": "Ford",
     "birth": "1990-10-10",
-    "eyes": "Zielony",
+    "eyes": "Niebieski",
     "registration": "AB1234"
+  },
+  {
+    "id": 9,
+    "name": "Ewelina",
+    "car": "Toyota",
+    "birth": "2000-01-01",
+    "eyes": "Zielony",
+    "registration": "CD5678"
   }
 ];
